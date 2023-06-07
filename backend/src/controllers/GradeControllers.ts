07f44d4b63369704b6e988ad08fe51577834bf33
Isaac Correia Gonçalves <isaac.correia.2406@gmail.com>
2023-06-07 18:32:42 -0300

    fixed route to get grade by professorId

--- backend/src/controllers/GradeControllers.ts
+++ backend/src/controllers/GradeControllers.ts
@@ -101,4 +101,84 @@ export class GradeController {
       return response.status(500).json({ message: 'internal server error' })
     }
   }
+  async getAgendamentosData(request: Request, response: Response) {
+    console.log("get grade");
+    const { semestre, professor_id } = request.body;
+    console.log(request.body);
+  
+    try {
+      // Retrieve grade data for the specified professor and semester
+      const query = `
+        SELECT 
+          grade.id, 
+          grade.horario_inicio, 
+          grade.horario_fim, 
+          grade.dia_da_semana, 
+          grade.semestre, 
+          grade.created_at, 
+          grade.updated_at, 
+          professores.name as professor, 
+          disciplinas.descricao as disciplina, 
+          laboratorios.descricao as laboratorio 
+        FROM 
+          grade 
+        LEFT JOIN 
+          professores ON grade.id_professor = professores.id 
+        LEFT JOIN 
+          disciplinas ON grade.id_disciplina = disciplinas.id 
+        LEFT JOIN 
+          laboratorios ON grade.id_sala = laboratorios.id 
+        WHERE 
+          grade.id_professor = '${professor_id}'
+        ORDER BY
+          grade.id ASC
+      `;
+      const gradeWithProfessor = await gradeRepositories.query(query);
+      // console.log(gradeWithProfessor);
+      
+      //coint the items in the array
+      const count = gradeWithProfessor.length;
+      // console.log(count);
+
+      // For each grade, retrieve associated agendamentos
+      const gradeWithAgendamento = await Promise.all(
+        gradeWithProfessor.map(async (grade: any) => {
+          const id_grade = grade.id;
+  
+          const queryAgendamento = `
+            SELECT
+              agendamento.id as id_agendamento,
+              date,
+              horario_inicio,
+              horario_fim,
+              id_professor,
+              id_grade,
+              id_laboratorio,
+              professores.name as professor,
+              laboratorios.descricao as laboratorio
+            FROM
+              agendamento
+            INNER JOIN
+              professores ON CAST(id_professor AS INTEGER) = professores.id
+            INNER JOIN
+              laboratorios ON CAST(id_laboratorio AS INTEGER) = laboratorios.id
+            WHERE
+              id_grade = '${id_grade}'
+          `;
+          const agendamentos = await gradeRepositories.query(queryAgendamento);
+  
+          grade.agendamentos = agendamentos || [];
+  
+          return grade;
+        })
+      );
+  
+      // console.log(gradeWithAgendamento);
+  
+      return response.status(200).json(gradeWithAgendamento);
+    } catch (error) {
+      console.log(error);
+      return response.status(500).json({ message: "internal server error" });
+    }
+  }
 }
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -23,7 +23,9 @@ routes.post('/grade/dashboard', new GradeController().getDashboardData)
 
 routes.get('/agendamento', new AgendamentoController().get)
 
-routes.post('/agendamento/grouped', new AgendamentoController().getGroupedById) // GroupByID
+routes.get('/grade/agendamentos', new AgendamentoController().getGroupedById) // GroupByID
+
+routes.post('/grade/agendamentos', new GradeController().getAgendamentosData)
 
 routes.post('/agendamento', new AgendamentoController().create)
 routes.put('/agendamento/:id', new AgendamentoController().update);
