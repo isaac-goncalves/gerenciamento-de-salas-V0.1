83f0b91d4c23aac904e903a9d99d38ae052a7d25
Isaac Correia Gonçalves <isaac.correia.2406@gmail.com>
2023-07-24 20:56:15 -0300

    Revert "route test commit"
    This reverts commit 6b99a86d55fa821c0cbff2f7af71ec4fb5cc52f3.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -19,7 +19,7 @@ function App() {
         <Routes>
           <Route path="/" element={
             <>
-              <Dashboard />
+              <LoginScreen />
               <Circles ballCount={4} />
             </>
           } />
