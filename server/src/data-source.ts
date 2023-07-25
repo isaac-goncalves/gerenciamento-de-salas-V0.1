8d5a3b71859acf967703ee3c76f616d86efe5d26
Isaac Correia Gonçalves <isaac.correia.2406@gmail.com>
2023-07-25 18:36:03 -0300

    added enviroment table variables to backend

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -3,15 +3,19 @@ import 'reflect-metadata'
 
 import { DataSource } from "typeorm";
 
+const host = process.env.DB_HOST;
 const port = process.env.DB_PORT as number | undefined;
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const database = process.env.DB_DATABASE;
 
 export const AppDataSource = new DataSource({
     port: port,
     type: "postgres",
-    host: "localhost",
-    username: "postgres",
-    password: "2406",
-    database: "gerenciamento-de-salas",
+    host: host,
+    username: username,
+    password: password,
+    database: database,
     entities:   [`${__dirname}/**/entities/*.{ts,js}`],
     migrations:   [`${__dirname}/**/migrations/*.{ts,js}`],
 })
