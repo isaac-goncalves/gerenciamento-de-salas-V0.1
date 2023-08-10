4a1d8bfc7548fe02837596cf59b3fd180843892d
Isaac Correia Gonçalves <isaac.correia.2406@gmail.com>
2023-08-09 22:41:07 -0300

    removed host to test

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
     watch: {
       usePolling: true,
     },
-    host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
     port: 5173, // you can replace this port with any port
   }
