6e3e021248da1eb4ec93d312aa780b4286797742
Isaac Correia Gonçalves <isaac.correia.2406@gmail.com>
2023-08-20 16:01:09 -0300

    trying

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Set the base to the root of the project
+  host: true,
   server: {
     watch: {
       usePolling: true,
