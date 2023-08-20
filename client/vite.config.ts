6a9843c192599f268c26598dd93bdb2daa9f3fc1
Isaac Correia Gonçalves <isaac.correia.2406@gmail.com>
2023-08-20 17:03:01 -0300

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,16 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     watch: {
       usePolling: true,
     },
     strictPort: true,
     port: 5173, // you can replace this port with any port
-  },
-  base: './'
+  }
 })
 
