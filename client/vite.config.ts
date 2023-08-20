64a59e2934cb3083471a6283f42bf47793e3f435
Isaac Correia Gonçalves <isaac.correia.2406@gmail.com>
2023-08-20 15:50:56 -0300

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Set the base to the root of the project
   server: {
     watch: {
       usePolling: true,
