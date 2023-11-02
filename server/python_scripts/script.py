389e9a9d01b5647b2ea49671854debbb2ea02b23
Isaac Correia Gonçalves <isaac.correia.2406@gmail.com>
2023-11-02 17:41:47 -0300

    feat: added correct template upload

--- server/python_scripts/script.py
+++ server/python_scripts/script.py
@@ -2,6 +2,10 @@ import sys
 import psycopg2
 import pandas as pd
 from datetime import datetime
+import time
+
+start_time = time.time()
+
 
 # Check if the file path argument is provided
 if len(sys.argv) < 2:
@@ -10,16 +14,29 @@ if len(sys.argv) < 2:
 
 # Get the file path from the command-line argument
 file_path = sys.argv[1]
-c 
-# print(course_id)
+course_id = int(sys.argv[2])
+print(course_id)
 # Establish a connection to your PostgreSQL database
 connection = psycopg2.connect(
-    host='localhost',
+    #host='localhost',
+    host = '192.168.0.53',
     port='5432',
     database='gerenciamento-de-salas',
     user='postgres',
     password='2406'
 )
+
+def deleteBasedOnID(tableName, course_id):
+    try:
+        query = "DELETE FROM " + tableName + " WHERE course_id = %s"
+        cursor.execute(query, (int(course_id),))
+        connection.commit()
+        count = cursor.rowcount
+        #print in red color 
+        print("\033[91m {}\033[00m" .format(count), "Record deleted FROM " + tableName + " successfully ")
+    except (Exception, psycopg2.Error) as error:
+        print("Error in Delete operation", error)
+    
 
 # Local Database
 # connection = psycopg2.connect(
@@ -51,55 +68,54 @@ dfSemestres.columns = ['descricao', 'ID']
 
 dfDiasSemana.columns = ['dia_da_semana','id']
                 
-dfGrade.dropna(subset=['id', 'horario_inicio', 'horario_fim', 'dia_da_semana', 'id_professor', 'id_disciplina', 'semestre', 'id_sala', 'created_at', 'updated_at'], inplace=True)
-dfDisciplinas.dropna(subset=['disciplina', 'id'], inplace=True)
-dfProfessores.dropna(subset=['name', 'id', 'surname','email', 'disciplina'], inplace=True)
-dfLaboratorio.dropna(subset=['descricao', 'id', 'capacidade'], inplace=True)
-dfSemestres.dropna(subset=['descricao', 'ID'], inplace=True)
-dfDiasSemana.dropna(subset=['dia_da_semana', 'id'], inplace=True)
+# dfGrade.dropna(subset=['id', 'horario_inicio', 'horario_fim', 'dia_da_semana', 'id_professor', 'id_disciplina', 'semestre', 'id_sala', 'created_at', 'updated_at'], inplace=True)
+# dfDisciplinas.dropna(subset=['disciplina', 'id'], inplace=True)
+# dfProfessores.dropna(subset=['name', 'id', 'surname','email', 'disciplina'], inplace=True)
+# dfLaboratorio.dropna(subset=['descricao', 'id', 'capacidade'], inplace=True)
+# dfSemestres.dropna(subset=['descricao', 'ID'], inplace=True)
+# dfDiasSemana.dropna(subset=['dia_da_semana', 'id'], inplace=True)
+
+dataframes = [dfGrade, dfDisciplinas, dfProfessores, dfLaboratorio, dfSemestres, dfDiasSemana]
+for df in dataframes:
+    df.dropna(inplace=True)
 
 # Access the data within the specified range
-print(dfGrade)
+# print(dfGrade)
 
-print(dfDisciplinas)
+# print(dfDisciplinas)
 
-print(dfProfessores)
+# print(dfProfessores)
 
-print(dfLaboratorio)
+# print(dfLaboratorio)
 
-print(dfSemestres)
+# print(dfSemestres)
 
-print(dfDiasSemana)
+# print(dfDiasSemana)
 
 
 # Create a cursor object
 cursor = connection.cursor()
 
 #------------------- disciplinas -------------------#
-queryTruncate = f"TRUNCATE disciplinas;"
+#queryTruncate = f"TRUNCATE disciplinas;"
+#cursor.execute(queryTruncate)
 
-cursor.execute(queryTruncate)
-
-reset_sequence_query = "ALTER SEQUENCE disciplinas_id_seq RESTART WITH 1;" # reset id sequence
-cursor.execute(reset_sequence_query)
+deleteBasedOnID("disciplinas", course_id)
+# reset id sequence
 
 for index, row in dfDisciplinas.iterrows():
-    descricao = row['disciplina']
+    descricao = row['disciplina'],
+    course_id_data = int(course_id)
+    query = f"INSERT INTO disciplinas (descricao, course_id) VALUES (%s, %s);" # Modify the table name as per your requirement
 
+    data = (descricao, course_id_data)
 
-    # Construct the SQL INSERT statement
-    query = f"INSERT INTO disciplinas (descricao) VALUES ('{descricao}');" # Modify the table name as per your requirement
+    cursor.execute(query, data) 
 
-    cursor.execute(query)
 
 #------------------- professores -------------------#
 
-queryTruncateProfessores = "TRUNCATE professores CASCADE;"
-cursor.execute(queryTruncateProfessores)
-
-# Reset the primary key sequence for professores table
-reset_sequence_query_professores = "ALTER SEQUENCE professores_id_seq RESTART WITH 1;"
-cursor.execute(reset_sequence_query_professores)
+deleteBasedOnID("professores", course_id)
 
 for index, row in dfProfessores.iterrows():
     name = row['name']
@@ -110,19 +126,15 @@ for index, row in dfProfessores.iterrows():
     updated_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # Current timestamp
 
     # Construct the SQL INSERT statement for professores
-    query_professores = f"INSERT INTO professores (name, surname, email, disciplina, created_at, updated_at) VALUES ('{name}', '{surname}','{email}', {disciplina}, '{created_at}', '{updated_at}');"
+    query_professores = f"INSERT INTO professores (course_id, name, surname, email, disciplina, created_at, updated_at) VALUES ('{course_id}','{name}', '{surname}','{email}', {disciplina}, '{created_at}', '{updated_at}');"
     cursor.execute(query_professores)
 
-query_set_professores_sequence = "SELECT setval('professores_id_seq', (SELECT MAX(id) FROM professores));"
-cursor.execute(query_set_professores_sequence)
+# query_set_professores_sequence = "SELECT setval('professores_id_seq', (SELECT MAX(id) FROM professores));"
+# cursor.execute(query_set_professores_sequence)
 
 #------------------- laboratorios -------------------#
 
-queryTruncateLaboratorio = "TRUNCATE laboratorios CASCADE;"
-cursor.execute(queryTruncateLaboratorio)
-
-reset_sequence_query_laboratorio = "ALTER SEQUENCE laboratorios_id_seq RESTART WITH 1;"
-cursor.execute(reset_sequence_query_laboratorio)
+# deleteBasedOnID("laboratorios", course_id)
 
 for index, row in dfLaboratorio.iterrows():
     descricao = row['descricao']
@@ -134,31 +146,25 @@ for index, row in dfLaboratorio.iterrows():
 
 #------------------- Semestres -------------------#
 
-queryTruncateSemestres = "TRUNCATE semestres CASCADE;"
-cursor.execute(queryTruncateSemestres)
-
-reset_sequence_query_semestres = "ALTER SEQUENCE semestres_id_seq RESTART WITH 1;"
-cursor.execute(reset_sequence_query_semestres)
+deleteBasedOnID("semestres", course_id)
 
 for index, row in dfSemestres.iterrows():
     descricao = row['descricao']
 
-    query_semestres = f"INSERT INTO semestres (descricao) VALUES ('{descricao}');"
+    query_semestres = f"INSERT INTO semestres (course_id, descricao) VALUES ('{course_id}','{descricao}');"
     cursor.execute(query_semestres)
 
 #------------------- Dias da Semana -------------------#
 
-queryTruncateDiasSemana = "TRUNCATE dias_da_semana CASCADE;"
-cursor.execute(queryTruncateDiasSemana)
+deleteBasedOnID("dias_da_semana", course_id)
 
-
-reset_sequence_query_dias_da_semana = "ALTER SEQUENCE dias_da_semana_id_seq RESTART WITH 1;"
-cursor.execute(reset_sequence_query_dias_da_semana)
+# reset_sequence_query_dias_da_semana = "ALTER SEQUENCE dias_da_semana_id_seq RESTART WITH 1;"
+# cursor.execute(reset_sequence_query_dias_da_semana)
 
 for index, row in dfDiasSemana.iterrows():
     dia_da_semana = row['dia_da_semana']
 
-    query_dias_da_semana = f"INSERT INTO dias_da_semana (dia_da_semana) VALUES ('{dia_da_semana}');"
+    query_dias_da_semana = f"INSERT INTO dias_da_semana (course_id, dia_da_semana) VALUES ('{course_id}', '{dia_da_semana}');"
     cursor.execute(query_dias_da_semana)
 
 #------------------- grade -------------------#
@@ -184,11 +190,12 @@ for index, row in dfGrade.iterrows():
     created_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # Current timestamp
     updated_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # Current timestamp
     
-    query_grade = "INSERT INTO grade (grade_id,horario_inicio, horario_fim, dia_da_semana, id_professor, id_disciplina, course_id, semestre, id_sala, created_at, updated_at) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);" # Modify the table name as per your requirement
+    query_grade = "INSERT INTO grade (grade_id, horario_inicio, horario_fim, dia_da_semana, id_professor, id_disciplina, course_id, semestre, id_sala, created_at, updated_at) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);" # Modify the table name as per your requirement
     
-    data = (grade_id,horario_inicio, horario_fim, dia_da_semana, id_professor, id_disciplina, course_id_data, semestre, id_sala, created_at, updated_at)
+    data = (grade_id, horario_inicio, horario_fim, dia_da_semana, id_professor, id_disciplina, course_id_data, semestre, id_sala, created_at, updated_at)
     
     cursor.execute(query_grade, data)
+    
     # print(course_id)
 
 #------------------- disciplinas -------------------#
@@ -209,9 +216,7 @@ queryGetLaboratorio = "SELECT * FROM laboratorio LIMIT 100;"  # Modify the table
 queryGetDiasSemana = "SELECT * FROM dias_semana LIMIT 100;"  # Modify the table name as per your requirement
 
 
-
 # Execute the query
-
 
 cursor.execute(queryGetGrade)
 
@@ -223,12 +228,20 @@ connection.commit()
 
 print("Checking!")
 # Print the fetched data
-for row in rows:
-   print(row)
+# for row in rows:
+#    print(row)
 
-print("Rows: ", len(rows))
-print("FINISHED!")
+# print("Rows: ", len(rows))
+#print in green 
+print("\033[92m {}\033[00m" .format(len(rows)), "Records inserted successfully into grade table")
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"Execution time: {elapsed_time} seconds")
 
 # Close the cursor and the connection
 cursor.close()
 connection.close()
+
+
+
