8dedec544b463d8bbdb1acbd3e63dbcc15ac4c0d
Isaac Correia Gonçalves <isaac.correia.2406@gmail.com>
2023-11-13 21:23:53 -0300

    added

---
+++ client/src/pages/Components/ParticlesComponent/index.tsx
@@ -0,0 +1,95 @@
+import React, { useCallback } from 'react'
+
+import Particles from "react-tsparticles";
+import { Container, Engine } from 'tsparticles-engine';
+import { loadSlim } from "tsparticles-slim";
+
+function index({ theme }: any) {
+
+  //PARTICLES FUNCTIONS
+  const particlesInit = useCallback(async (engine: Engine) => {
+    // console.log(engine);
+
+    // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    //await loadFull(engine);
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container: Container | undefined) => {
+    // await console.log(container);
+  }, []);
+
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        // background: {
+        //   color: {
+        //     // value: "#ffffff",
+        //   },
+        // },
+        fpsLimit: 120,
+        // interactivity: {
+        //   events: {
+        //     onClick: {
+        //       enable: true,
+        //       mode: "push",
+        //     },
+        //     onHover: {
+        //       enable: true,
+        //       mode: "repulse",
+        //     },
+        //     resize: true,
+        //   },
+        //   modes: {
+        //     push: {
+        //       quantity: 4,
+        //     },
+        //     repulse: {
+        //       distance: 200,
+        //       duration: 0.4,
+        //     },
+        //   },
+        // },
+        particles: {
+          color: {
+            value: theme.bubblecolor,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 3,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 1200,
+            },
+            value: 10,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 200, max: 200 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  )
+}
+
+export default index
--- client/src/pages/Grade/index.tsx
+++ client/src/pages/Grade/index.tsx
@@ -28,9 +28,7 @@ import { ParticleOptions } from '../Components/ParticlesOptions';
 
 import { useCallback } from "react";
 import { Theme, type Container, type Engine } from "tsparticles-engine";
-import Particles from "react-tsparticles";
-//import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
+
 //
 
 import { MdKeyboardArrowRight, MdKeyboardDoubleArrowRight, MdOutlineModeEdit, MdOutlineModeEditOutline, MdSubdirectoryArrowRight, MdToday } from 'react-icons/md';
@@ -46,6 +44,7 @@ import { on } from 'events';
 
 import banner from '../../../public/images/banner.jpg';
 import ModalAskSemestre from '../Components/ModalAskSemestre';
+import ParticlesComponent from '../Components/ParticlesComponent';
 
 
 interface ScheduleItem {
@@ -828,21 +827,6 @@ const Grade: any = ({ theme, themeName }: any) => {
     )
   }
 
-  //PARTICLES FUNCTIONS
-  const particlesInit = useCallback(async (engine: Engine) => {
-    console.log(engine);
-
-    // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    //await loadFull(engine);
-    await loadSlim(engine);
-  }, []);
-
-  const particlesLoaded = useCallback(async (container: Container | undefined) => {
-    await console.log(container);
-  }, []);
-
   //THEME FUNCTIONS
 
   function getThemeBasedClass(theme: string) {
@@ -868,80 +852,7 @@ const Grade: any = ({ theme, themeName }: any) => {
   //RENDERS -------------------------------------------------------------------------
   return (
     <>
-      <Particles
-        id="tsparticles"
-        init={particlesInit}
-        loaded={particlesLoaded}
-        options={{
-          // background: {
-          //   color: {
-          //     // value: "#ffffff",
-          //   },
-          // },
-          fpsLimit: 120,
-          interactivity: {
-            events: {
-              onClick: {
-                enable: true,
-                mode: "push",
-              },
-              onHover: {
-                enable: true,
-                mode: "repulse",
-              },
-              resize: true,
-            },
-            modes: {
-              push: {
-                quantity: 4,
-              },
-              repulse: {
-                distance: 200,
-                duration: 0.4,
-              },
-            },
-          },
-          particles: {
-            color: {
-              value: theme.mainpurple,
-            },
-            links: {
-              color: theme.mainpurple,
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
-            move: {
-              direction: "none",
-              enable: true,
-              outModes: {
-                default: "bounce",
-              },
-              random: false,
-              speed: 6,
-              straight: false,
-            },
-            number: {
-              density: {
-                enable: true,
-                area: 800,
-              },
-              value: 80,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: "circle",
-            },
-            size: {
-              value: { min: 1, max: 5 },
-            },
-          },
-          detectRetina: true,
-        }}
-      />
+      <ParticlesComponent theme={theme} />
       <ModalAgendamento action={userIsScheduling ? "CREATE" : "OPEN"} isVisible={schedulingModalIsVisible} onClose={handleCloseModalEdit} initialData={editedData} daysIds={daysIds} idUserLogado={userData.userData.id} userRole={userData.userData.role} />
       <ModalAskSemestre isVisible={askSemesterModalIsVisible} onCloseModalAskSemester={handleCloseModalAskSemester} />
       <ToastContainer
